79efed809167d8954d2fdc260d2a9d11570959a4
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-12-15 16:16:20 +0100

    Fix stores again

--- src/stores/themestore.ts
+++ src/stores/themestore.ts
@@ -1,18 +1,18 @@
+import { browser } from "$app/env";
 import { writable } from "svelte/store";
-import { onMount } from 'svelte';
-let theme = writable<string>("light");
-onMount(() => {
-    const preferDark = window.matchMedia("(prefers-color-scheme: dark)");
-    const settedTheme = window.localStorage.getItem("theme");
-    theme = writable<string>(settedTheme || (preferDark.matches ? "dark" : "light"));
-    let firstTime = true;
+
+const preferDark = browser && window.matchMedia("(prefers-color-scheme: dark)");
+const settedTheme = browser ? localStorage.getItem("theme") : "light";
+const theme = writable<string>(settedTheme || (preferDark.matches ? "dark" : "light"));
+let firstTime = true;
+if (browser) {
     theme.subscribe((val) => {
         if (firstTime) {
             firstTime = false;
             return;
         }
-        window.localStorage.setItem("theme", val);
+        localStorage.setItem("theme", val);
     });
-});
+}
 
 export default theme;
--- src/stores/todostore.ts
+++ src/stores/todostore.ts
@@ -1,10 +1,11 @@
+import { browser } from "$app/env";
 import type { TodoList } from "src/types/todo.types";
-import { Writable, writable } from "svelte/store";
-import { onMount } from 'svelte';
-let todos: Writable<TodoList[]> = writable<TodoList[]>([]);
-onMount(() => {
-    const localTodos: TodoList[] = JSON.parse(localStorage.getItem("todos")) as TodoList[] || [];
-    todos = writable<TodoList[]>(localTodos);
+import { writable } from "svelte/store";
+
+const localTodos: TodoList[] = browser ? JSON.parse(localStorage.getItem("todos")) as TodoList[] || [] : [];
+const todos = writable<TodoList[]>(localTodos);
+if (browser) {
     todos.subscribe((newVal: TodoList[]) => localStorage.setItem("todos", JSON.stringify(newVal)));
-});
+}
+
 export default todos;

a4e95e60d742fa80b6eb192cc271e56693072b0f
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-01-06 20:38:19 +0100

    Remove theme switcher

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,27 +2,12 @@
     import { Button } from "attractions";
 
     import logo from "../assets/dotoo.svg";
-    import theme from "../stores/themestore";
     import Icon from "./Icon.svelte";
-
-    const iconMapping = {
-        light: "brightness_4",
-        dark: "brightness_5",
-    };
-
-    const toggleTheme = () => {
-        $theme = $theme === "light" ? "dark" : "light";
-    };
 </script>
 
 <header>
     <img src={logo} alt="logo" />
     <span>DoToo</span>
-    <div class="theme-toggler">
-        <Button filled round on:click={toggleTheme}>
-            <Icon icon={iconMapping[$theme]} />
-        </Button>
-    </div>
 </header>
 
 <style lang="scss">
@@ -42,9 +27,5 @@
     span {
         font-weight: bold;
         color: theme.$main-text;
-    }
-    .theme-toggler {
-        justify-self: end;
-        padding: 0 1rem;
     }
 </style>
